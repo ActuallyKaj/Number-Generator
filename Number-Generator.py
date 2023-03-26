a7866c71e95a059005b8f8db1ab308c0dd5abfc1
Kuba Kluczynski <Kuba.Kluczynski07@gmail.com>
2023-03-26 20:41:22 +0100

    Number Generator
    Added the main code to calculate the user's range for the number they want generated randomly

--- Number-Generator.py
+++ Number-Generator.py
@@ -1,1 +1,10 @@
-print("hello")
+import numpy
+
+lowerBound = int(input("Please enter the lower bound: "))
+upperBound = int(input("Please enter the upper bound: "))
+
+low = lowerBound
+high = upperBound + 1
+
+result = np.random.randint(low, high)
+print(result)
